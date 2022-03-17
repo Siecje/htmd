d8942a0d3d9d069181b9355e2abfd95e6b2ca50c
Cody Scott <cody.j.b.scott@gmail.com>
2022-03-17 16:26:34 -0400

    add utils.py

--- htmd/cli.py
+++ htmd/cli.py
@@ -1,82 +1,15 @@
 from importlib import reload
 import os
-import shutil
 import sys
 
 import click
-from csscompressor import compress
-from flask import Flask
-from jsmin import jsmin
+
+from .utils import combine_and_minify_css, combine_and_minify_js, copy_file, copy_missing_templates, create_directory
 
 
 @click.group()
 def cli():
     pass # pragma: no cover
-
-
-def create_directory(name):
-    try:
-        os.mkdir(name)
-    except FileExistsError:
-        click.echo(click.style(f'{name} already exists and was not created.', fg='red'))
-    else:
-        click.echo(click.style(f'{name} was created.', fg='green'))
-
-
-def copy_file(src, dest):
-    if os.path.exists(dest) is False:
-        shutil.copyfile(src, dest)
-        click.echo(click.style(f'{dest} was created.', fg='green'))
-    else:
-        click.echo(click.style(f'{dest} already exists and was not created.', fg='red'))
-
-
-def combine_and_minify_css(static_folder):
-    # Combine and minify all .css files in the static folder
-    css_files = sorted([f for f in os.listdir(static_folder) if f.endswith('.css') and f != 'combined.min.css'])
-    if not css_files:
-        # There are no .css files in the static folder
-        return
-
-    with open(os.path.join(static_folder, 'combined.min.css'), 'w') as master:
-        for f in css_files:
-            with open(os.path.join(static_folder, f), 'r') as css_file:
-                # combine all .css files into one
-                master.write(css_file.read())
-
-    with open(os.path.join(static_folder, 'combined.min.css'), 'r') as master:
-        combined = master.read()
-    with open(os.path.join(static_folder, 'combined.min.css'), 'w') as master:
-        master.write(compress(combined))
-
-
-def combine_and_minify_js(static_folder):
-    # Combine and minify all .js files in the static folder
-    js_files = sorted([f for f in os.listdir(static_folder) if f.endswith('.js') and f != 'combined.min.js'])
-    if not js_files:
-        # There are no .js files in the static folder
-        return
-
-    with open(os.path.join(static_folder, 'combined.min.js'), 'w') as master:
-        for f in js_files:
-            with open(os.path.join(static_folder, f), 'r') as js_file:
-                # combine all .js files into one
-                master.write(js_file.read())
-
-    # minify should be done after combined to avoid duplicate identifiers
-    # minifying each file will use 'a' for the first identifier
-    with open(os.path.join(static_folder, 'combined.min.js'), 'r') as master:
-        combined = master.read()
-    with open(os.path.join(static_folder, 'combined.min.js'), 'w') as master:
-        master.write(jsmin(combined))
-
-
-def copy_missing_templates():
-    htmd_dir = os.path.dirname(__file__)
-    template_dir = os.path.join(htmd_dir, 'templates')
-    for template_file in os.listdir(template_dir):
-        copy_file(os.path.join(template_dir, template_file),
-            os.path.join('templates', template_file))
 
 
 @cli.command('start', short_help='Create example files to get started.')
---
+++ htmd/utils.py
@@ -0,0 +1,71 @@
+import os
+import shutil
+
+import click
+from csscompressor import compress
+from jsmin import jsmin
+
+
+def create_directory(name):
+    try:
+        os.mkdir(name)
+    except FileExistsError:
+        click.echo(click.style(f'{name} already exists and was not created.', fg='red'))
+    else:
+        click.echo(click.style(f'{name} was created.', fg='green'))
+
+
+def copy_file(src, dest):
+    if os.path.exists(dest) is False:
+        shutil.copyfile(src, dest)
+        click.echo(click.style(f'{dest} was created.', fg='green'))
+    else:
+        click.echo(click.style(f'{dest} already exists and was not created.', fg='red'))
+
+
+def combine_and_minify_css(static_folder):
+    # Combine and minify all .css files in the static folder
+    css_files = sorted([f for f in os.listdir(static_folder) if f.endswith('.css') and f != 'combined.min.css'])
+    if not css_files:
+        # There are no .css files in the static folder
+        return
+
+    with open(os.path.join(static_folder, 'combined.min.css'), 'w') as master:
+        for f in css_files:
+            with open(os.path.join(static_folder, f), 'r') as css_file:
+                # combine all .css files into one
+                master.write(css_file.read())
+
+    with open(os.path.join(static_folder, 'combined.min.css'), 'r') as master:
+        combined = master.read()
+    with open(os.path.join(static_folder, 'combined.min.css'), 'w') as master:
+        master.write(compress(combined))
+
+
+def combine_and_minify_js(static_folder):
+    # Combine and minify all .js files in the static folder
+    js_files = sorted([f for f in os.listdir(static_folder) if f.endswith('.js') and f != 'combined.min.js'])
+    if not js_files:
+        # There are no .js files in the static folder
+        return
+
+    with open(os.path.join(static_folder, 'combined.min.js'), 'w') as master:
+        for f in js_files:
+            with open(os.path.join(static_folder, f), 'r') as js_file:
+                # combine all .js files into one
+                master.write(js_file.read())
+
+    # minify should be done after combined to avoid duplicate identifiers
+    # minifying each file will use 'a' for the first identifier
+    with open(os.path.join(static_folder, 'combined.min.js'), 'r') as master:
+        combined = master.read()
+    with open(os.path.join(static_folder, 'combined.min.js'), 'w') as master:
+        master.write(jsmin(combined))
+
+
+def copy_missing_templates():
+    htmd_dir = os.path.dirname(__file__)
+    template_dir = os.path.join(htmd_dir, 'templates')
+    for template_file in os.listdir(template_dir):
+        copy_file(os.path.join(template_dir, template_file),
+            os.path.join('templates', template_file))
