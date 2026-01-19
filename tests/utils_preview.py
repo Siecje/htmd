b783a4b819d415852f9adbec5636f14f11315b5d
Cody Scott <cody.j.b.scott@gmail.com>
2026-01-19 07:31:53 -0500

    Refactor run_preview()
    - Use event instead of polling to know when `htmd preview` is ready
    - Allows immediate port reuse, bypassing TCP TIME_WAIT

--- tests/utils_preview.py
+++ tests/utils_preview.py
@@ -1,13 +1,12 @@
 from collections.abc import Generator
 from contextlib import contextmanager
+import socket
 import threading
-import time
 from unittest.mock import patch
 
 from click.testing import CliRunner
 from flask import Flask
 import htmd.cli.preview as preview_module
-import requests
 from werkzeug.serving import BaseWSGIServer, make_server
 
 
@@ -19,10 +18,10 @@ def run_preview(
     runner: CliRunner,
     args: list[str] | None = None,
     *,
-    max_tries: int = 1_000,
     threaded:  bool = False,
     webserver_collector: list | None = None,
 ) -> Generator[str]:
+    preview_ready = threading.Event()
     def create_webserver(app: Flask, host: str, port: int) -> BaseWSGIServer:
         webserver = make_server(
             host,
@@ -30,25 +29,41 @@ def run_preview(
             app,
             threaded=threaded,
         )
-        assert webserver is not None
         if webserver_collector is not None:
             webserver_collector.append(webserver)
+        assert webserver is not None
+
+        # Prevent "Address already in use" errors when running tests back-to-back
+        # by bypassing the OS's TCP TIME_WAIT cooldown period.
+        webserver.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        # Replace serve_forever() so our event is set
+        # as close to when the webserver is available
+        original_serve_forever = webserver.serve_forever
+        def _serve_forever(poll_interval: float = 0.5) -> None:
+            preview_ready.set()
+            original_serve_forever(poll_interval=poll_interval)
+        webserver.serve_forever = _serve_forever  # type: ignore[method-assign]
+
         return webserver
 
     stop_event = threading.Event()
     with (
-        # start_webserver is replaced so it can handle each request in a thread
+        # start_webserver is replaced
+        # so it can handle each request in a thread
+        # and we get an event on server_forever()
         patch(
             'htmd.cli.preview.create_webserver',
             side_effect=create_webserver,
         ),
-        # create_stop_event is replace so we can stop preview
-        # since we can't send signals (ctrl+c) to a thread
+        # create_stop_event is replaced so we can stop preview
+        # since we can't send signals (ex. ctrl+c) to a thread
         patch(
             'htmd.cli.preview.create_stop_event',
             return_value=stop_event,
         ),
-        # setup_stop_thread_on_signal is replaced because threads can't receive signals
+        # setup_stop_thread_on_signal is replaced
+        # because threads can't receive signals
         patch(
             'htmd.cli.preview.set_stop_event_on_signal',
             side_effect=lambda _x: None,
@@ -61,19 +76,10 @@ def run_preview(
             daemon=True,
         )
         thread.start()
+
         try:
-            for _ in range(max_tries):  # pragma: no branch
-                try:
-                    requests.head(BASE_URL, timeout=1)
-                    break
-                except (
-                    requests.exceptions.ConnectionError,
-                    requests.exceptions.Timeout,
-                ):  # pragma: no cover
-                    pass
-
-                time.sleep(0.1)
-
+            # This is set right before the webserver starts
+            preview_ready.wait()
             yield BASE_URL
         finally:
             # Trigger preview to stop
